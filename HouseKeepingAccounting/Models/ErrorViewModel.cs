2bab7c859a04fc9c9397c702c2c118b757731d42
Lazarev <myjekmail@ya.ru>
2018-10-18 14:56:17 +0500

    Добавьте файлы проекта.

---
+++ HouseKeepingAccounting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HouseKeepingAccounting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
